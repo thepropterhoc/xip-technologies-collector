039569d31c09275640b95dfe2672213bc7956fbb
Shelby <vanhooser@ou.edu>
2014-09-01 23:50:23 -0500

    Added requests

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import serial, xbee
+import serial, xbee, requests
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.close()
 ser.open()
 xb = xbee.ZigBee(ser)
 
+baseURL = "http://www.projectxip.com/api/lot/"
+
 while True:
 	frame = ser.readline().strip().split(' ')
 	if frame[0] == "INCR":
-		print "Increment of lot: " + frame[1]
+		print requests.post(baseURL + "increment/"+ frame[1])
+		#print "Increment of lot: " + frame[1]
 	elif frame[1] == "DECR":
-		print "Decrement of lot: " + frame[1]
+		print requests.post(baseURL + "decrement/" + frame[1])
+		#print "Decrement of lot: "  + frame[1]
 	
