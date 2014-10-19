4e188048af5963fbd967fb6108834e8b1950d9c8
Shelby Vanhooser <vanhooser@ou.edu>
2014-10-19 02:30:02 +0000

    Decrement fixed

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ xb = xbee.ZigBee(ser)
 
 baseURL = "http://projectxip.com/api/lot/"
 
-while True:
-	frame = ser.readline().strip().split(' ')
-	if frame[0] == "INCR":
-		print requests.post(baseURL + "increment/"+ frame[1])
-		#print "Increment of lot: " + frame[1]
-	elif frame[1] == "DECR":
-		print requests.post(baseURL + "decrement/" + frame[1])
-		#print "Decrement of lot: "  + frame[1]
-	
+with open('log.txt', 'a') as f:
+	while True:
+		frame = ser.readline().strip().split(' ')
+		if frame[0] == "INCR":
+			try:
+				print requests.post(baseURL + "increment/"+ frame[1])
+			except:
+				f.write("Failed to push increment")
+			f.write("Increment of lot: " + frame[1])
+		elif frame[0] == "DECR":
+			print requests.post(baseURL + "decrement/" + frame[1])
+			try:
+				print "Decrement of lot: "  + frame[1]
+			except:
+				f.write("Failed to push decrement")
+			f.write("Decrement of lot: " + frame[1])
