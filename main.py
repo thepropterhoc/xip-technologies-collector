87de092a47ad519005cf5aeb7cee3644d6cd7848
Shelby <vanhooser@ou.edu>
2014-10-24 12:14:22 -0500

    Multithreading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import serial, xbee, requests
+import serial, xbee, requests, threading, time
+from Queue import Queue as q 
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.close()
@@ -7,12 +8,22 @@ xb = xbee.ZigBee(ser)
 
 baseURL = "http://projectxip.com/api/lot/"
 
+frames = q()
+
+class reader(threading.Thread):
+	def run(self):
+		while True:
+			frame = frames.get()
+			if frame[0] == "INCR":
+				print requests.post(baseURL + "increment/"+ frame[1])
+				print "Increment of lot: " + frame[1]
+			elif frame[0] == "DECR":
+				print requests.post(baseURL + "decrement/" + frame[1])
+				print "Decrement of lot: "  + frame[1]
+		
+read = reader()
+read.start()
+
 while True:
 	frame = ser.readline().strip().split(' ')
-	if frame[0] == "INCR":
-		print requests.post(baseURL + "increment/"+ frame[1])
-		#print "Increment of lot: " + frame[1]
-	elif frame[1] == "DECR":
-		print requests.post(baseURL + "decrement/" + frame[1])
-		#print "Decrement of lot: "  + frame[1]
-	
+	frames.put(frame)
